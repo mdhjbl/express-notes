6e02e54d2da397695414c987ff3b113a0f08257a
king <154808937+mdh058@users.noreply.github.com>
2025-07-17 16:08:53 +0330

    Add simple API to get language by its ID

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+//!hello world basic api
+// app.get('/' , ( req, res)=>{
+//     res.send("Hello World :)")
+    
+// })
+
+//!getting data with id from the client
+/*app.get('/languages/:id', (req, res) => {
+    const languages = [
+        { "id": 1, "name": "English", "rate": 4.5 },
+        { "id": 2, "name": "French",  "rate": 3.8 },
+        { "id": 3, "name": "Russian", "rate": 4.8 }
+    ]
+
+    const language = languages.find(lang => lang.id == Number(req.params.id))
+    if (language) {
+        res.send(language)
+    } else {
+        res.send("The language not found :(")
+    }
+})*/
+
+
+
+
+
+
+app.listen(3000 , ()=>{
+    console.log("server is running on port 3000 ")
+})
